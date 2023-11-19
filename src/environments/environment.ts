d39c873b32069f2152595cf28467fff26c4a5e0a
Petcu98 <florinpetcu980@gmail.com>
2023-11-19 09:19:51 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Car Parts Shop",
+  appOwner: "Catalin Florin Petcu",
+  appLogo: "https://cdn.vectorstock.com/i/preview-1x/88/41/auto-parts-store-car-logo-emblem-vector-25328841.jpg"
+};
